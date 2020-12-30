72a59e66090d82e54b95789e789c5203d620e619
uycuhnt2467 <uycuhnt2467@gmail.com>
2020-12-30 17:58:41 +0800

    Binary Search: 1011

---
+++ binary_search/1011.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 30 17:16:41 2020
+
+@author: a8520
+"""
+
+
+# A conveyor belt has packages that must be shipped from one port to another 
+# within D days.
+
+# The i-th package on the conveyor belt has a weight of weights[i].  
+# Each day, we load the ship with packages on the conveyor belt (in the order 
+# given by weights).
+
+# We may not load more weight than the maximum weight capacity of the ship.
+
+# Return the least weight capacity of the ship that will result in all the packages on 
+# the conveyor belt being shipped within D days.
+
+# Example 1:
+# Input: weights = [1,2,3,4,5,6,7,8,9,10], D = 5
+# Output: 15
+
+# Example 2:
+# Input: weights = [3,2,2,4,1,4], D = 3
+# Output: 6
+
+# Example 3:
+# Input: weights = [1,2,3,1,1], D = 4
+# Output: 3
+class Solution(object):
+    def shipWithinDays(self, weights, D):
+        """
+        :type weights: List[int]
+        :type D: int
+        :rtype: int
+        """
+        def checkDay(w):
+            day = 0
+            i = 0
+            cur_w = w
+            while i < len(weights):
+                if cur_w - weights[i] >= 0:
+                    cur_w -= weights[i]
+                    i += 1
+                else:
+                    day += 1
+                    cur_w = w
+            day += 1
+            return day <= D
+        
+        l = max(weights)
+        r = sum(weights)
+        
+        while l < r:
+            mid = l + (r - l)//2
+            if checkDay(mid):
+                # can load less
+                r = mid
+            else:
+                # can load more
+                l = mid + 1
+        
+        return r
+
+sol = Solution()
+weights = [1,2,3,4,5,6,7,8,9,10]
+D = 5
+
+print(sol.shipWithinDays(weights, D))
+
+sol = Solution()
+weights = [3,2,2,4,1,4]
+D = 3
+print(sol.shipWithinDays(weights, D))        
+                    
+
+weights = [1,2,3,1,1]
+D = 4
+print(sol.shipWithinDays(weights, D))        
+        
+
+weights = [2,99]
+D = 4
+print(sol.shipWithinDays(weights, D))        
+        
+        
+        
+        
+        
+        
+        
--- binary_search/875.py
+++ binary_search/875.py
@@ -7,12 +7,75 @@ Created on Wed Dec 30 15:07:18 2020
 
 
 # 75. Koko Eating Bananas
+# Koko loves to eat bananas.  There are N piles of bananas, 
+# the i-th pile has piles[i] bananas.  
+# The guards have gone and will come back in H hours.
+# Koko can decide her bananas-per-hour eating speed of K.  
+# Each hour, she chooses some pile of bananas, and eats K bananas from that 
+# pile.  
+# If the pile has less than K bananas, she eats all of them instead, 
+# and won't eat any more bananas during this hour.
+# Koko likes to eat slowly, but still wants to finish eating all the bananas 
+# before the guards come back.
+# Return the minimum integer K such that she can eat all the bananas within H hours.
+
+# Example 1:
+# input: piles = [3,6,7,11], H = 8
+# output: 4
+
+# Example 2:
+# input: piles = [30,11,23,4,20], H = 5
+# output: 30
+
+# Example 3:
+# input: piles = [30,11,23,4,20], H = 6
+# output: 23
 
 
 class Solution(object):
+    
     def minEatingSpeed(self, piles, H):
         """
         :type piles: List[int]
         :type H: int
         :rtype: int
         """
+        
+        def checkTime(K, piles, H):
+            import math
+            time = 0
+            for i in piles:
+                time += math.ceil(i/K)
+                
+            return time <= H
+        
+        # new_piles = quick_sort(piles)
+        
+        piles.sort()
+        l = 1
+        r = max(piles)
+        
+        while l < r:
+            mid = l + (r - l) // 2
+            if checkTime(mid, piles, H):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
+
+            
+            
+test = Solution()
+piles = [3,6,7,11]
+H = 8
+print(test.minEatingSpeed(piles, 8))
+            
+            
+            
+            
+            
+            
+            
+            
+            
