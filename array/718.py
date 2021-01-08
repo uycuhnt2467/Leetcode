c7adf44dcc850478c90c70408591949321edd8c0
uycuhnt2467 <uycuhnt2467@gmail.com>
2021-01-08 10:09:12 +0800

    718. Maximum Length of Repeated Subarray

---
+++ array/718.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 08:58:26 2021
+
+@author: a8520
+"""
+
+
+class Solution(object):
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        
